5a14930b7586c7f2fe3ec8dc0dba351f6068b175
DenisEvdokumov <denis.vd@rambler.ru>
2017-12-27 19:45:01 +0200

    added new database test

--- Encrypt_itTest/UnitTest1.cs
+++ Encrypt_itTest/UnitTest1.cs
@@ -10,7 +10,7 @@ namespace Encrypt_itTest
     {
         class testclass
         {
-
+           
         } 
 
         [TestMethod]
@@ -19,6 +19,19 @@ namespace Encrypt_itTest
             Assert.AreEqual(1, encrypt.caseMethod("s1"));
             Assert.AreEqual(5, encrypt.caseMethod("s5"));
             Assert.AreEqual(8, encrypt.caseMethod("s8"));
+        }
+
+        [TestMethod]
+        public void DatabaseTestMethod()
+        {
+            Customer c0 = new Customer(0, 0, 0);
+            Customer c1 = new Customer(1, 1, 1);
+            Customer c5 = new Customer(0, 5, 5);
+
+            Assert.AreEqual("", c0.caseMethod());
+            Assert.AreEqual("3DES", c1.caseMethod());
+            Assert.AreEqual("IDEA", c5.caseMethod());
+
         }
     }
 }
